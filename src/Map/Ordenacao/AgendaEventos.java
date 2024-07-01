82e2fd2e53788846eb40c8560434ad20a6d5de28
Thais-Westphal <thais.westphal12@gmail.com>
2024-07-01 20:22:45 -0300

    Exemplos DIO
    Signed-off-by: Thais-Westphal <thais.westphal12@gmail.com>

--- src/Map/Ordenacao/AgendaEventos.java
+++ src/Map/Ordenacao/AgendaEventos.java
@@ -51,15 +51,11 @@ public class AgendaEventos
         agendaEventos.adicionarEvento(LocalDate.of(2022, Month.JULY, 15), "Evento 1", "Atração 1");
         agendaEventos.adicionarEvento(LocalDate.of(2022, 7, 9), "Evento 2", "Atração 2");
         agendaEventos.adicionarEvento(LocalDate.of(2000, Month.JANUARY, 10), "Evento 3", "Atração 3");
-       //agendaEventos.adicionarEvento(LocalDate.of(2024, 7, 1), "Evento 4", "Atração 4");
+        agendaEventos.adicionarEvento(LocalDate.of(2024, 7, 1), "Evento 4", "Atração 4");
         agendaEventos.adicionarEvento(LocalDate.of(2024, 7, 2), "Evento 5", "Atração 5");
-
 
         agendaEventos.exibirAgenda();
 
         agendaEventos.obterProximoEvento();
-
-
-
     }
 }
